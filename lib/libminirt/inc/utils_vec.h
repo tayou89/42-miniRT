70f527cad9e2eeff2cbb4e0254ca944b3cc7b12c
Jeonghwan Hwang <jhwang@c2r3s2.42seoul.kr>
2023-08-28 17:48:09 +0900

    add vector definition

--- lib/libminirt/inc/utils_vec.h
+++ lib/libminirt/inc/utils_vec.h
@@ -6,8 +6,7 @@
 
 t_vec3	vinit(void);
 t_vec3	vset(double x, double y, double z);
-t_point	pset(double x, double y, double z);
-t_color	cset(double x, double y, double z);
+t_vec3	vinverse(t_vec3 vec);
 int		vsame(t_vec3 vec1, t_vec3 vec2);	// 두벡터가 같은 벡터인지 확인하는 함수 추가
 double	vlen2(t_vec3 vec);
 double	vlen(t_vec3 vec);
@@ -18,6 +17,7 @@ t_vec3	vmul_v(t_vec3 vec1, t_vec3 vec2);
 t_vec3	vadd_s(t_vec3 vec, double s);
 t_vec3	vsub_s(t_vec3 vec, double s);
 t_vec3	vmul_s(t_vec3 vec, double s);
+t_vec3	vdiv_s(t_vec3 vec, double s);
 double	vdot(t_vec3 vec1, t_vec3 vec2);
 t_vec3	vcross(t_vec3 vec1, t_vec3 vec2);
 #endif
