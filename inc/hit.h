ab558ba7779d8ef5c15ae39588c6df65ec48c964
Jeonghwan Hwang <jhwang@c2r8s2.42seoul.kr>
2023-09-01 17:22:26 +0900

    fix parameter data type

--- inc/hit.h
+++ inc/hit.h
@@ -10,6 +10,7 @@ int	pl_hit(t_list *obj, t_ray ray, t_rec *rec);
 int	sp_hit(t_list *obj, t_ray ray, t_rec *rec);
 int	cy_hit(t_list *obj, t_ray ray, t_rec *rec);
 int	cy_hit_surface(t_cy *cy, t_ray ray, t_rec *rec);
-int	hit_object(t_list *world, t_ray primary, t_rec *rec);
+int	hit_object(t_data *data, t_ray primary, t_rec *rec);
 int	color_get_trgb(int t, int r, int g, int b);
+t_vec3	phong_lighting(t_rec *rec, t_data *scene);
 #endif
--- src/hit.c
+++ src/hit.c
@@ -14,18 +14,21 @@ static void	set_hit_func(int (*hit_func[])(t_list *, t_ray, t_rec *))
 	hit_func[CYLINDER] = cy_hit;
 }
 
-int	hit_object(t_list *world, t_ray primary, t_rec *rec)
+int	hit_object(t_data *data, t_ray primary, t_rec *rec)
 {
+	t_list	*world;
 	t_vec3	color;
 	int		(*hit_func[3])(t_list *, t_ray, t_rec *);
 
 	color = vset(0, 0, 0);
 	set_hit_func(hit_func);
+	world = data->obj;
 	while (world)
 	{
 		if (hit_func[world->type](world, primary, rec))
-			color = rec->albedo;
+			color = phong_lighting(rec, data);
 		world = world->next;
 	}
-	return (color_get_trgb(0x0, color.x, color.y, color.z));
+	color = vmul_s(color, 255.999);
+	return (color_get_trgb(0x0, (int)color.x, (int)color.y, (int)color.z));
 }
