522bccba98b01cafa4fce1eab10a0aad41b4ab97
Jeonghwan Hwang <jhwang@c2r8s2.42seoul.kr>
2023-09-01 18:50:17 +0900

    convert input data (normal, color)

--- src/parsing/ft_make_object_node.c
+++ src/parsing/ft_make_object_node.c
@@ -9,6 +9,7 @@ t_sp	*make_sphere_node(t_info *info, t_parsing *parsing)
 		ft_parsing_error(MALLOC_ERROR, 1, parsing);
 	sphere->center = get_coordinate_data(info->coordinate, parsing);
 	sphere->color = get_color_data(info->color, parsing);
+	sphere->color = vdiv_s(sphere->color, 255.0);
 	sphere->diameter = get_double(info->diameter, parsing);
 	sphere->radius = sphere->diameter / 2;
 	sphere->radius2 = sphere->radius * sphere->radius;
@@ -23,8 +24,9 @@ t_pl	*make_plane_node(t_info *info, t_parsing *parsing)
 	if (plane == (void *) 0)
 		ft_parsing_error(MALLOC_ERROR, 1, parsing);
 	plane->center = get_coordinate_data(info->coordinate, parsing);
-	plane->normal = get_vector_data(info->vector, parsing);
+	plane->normal = vunit(get_vector_data(info->vector, parsing));
 	plane->color = get_color_data(info->color, parsing);
+	plane->color = vdiv_s(plane->color, 255.0);
 	return (plane);
 }
 
@@ -36,8 +38,9 @@ t_cy	*make_cylinder_node(t_info *info, t_parsing *parsing)
 	if (cylinder == (void *) 0)
 		ft_parsing_error(MALLOC_ERROR, 1, parsing);
 	cylinder->center = get_coordinate_data(info->coordinate, parsing);
-	cylinder->normal = get_vector_data(info->vector, parsing);
+	cylinder->normal = vunit(get_vector_data(info->vector, parsing));
 	cylinder->color = get_color_data(info->color, parsing);
+	cylinder->color = vdiv_s(cylinder->color, 255.0);
 	cylinder->diameter = get_double(info->diameter, parsing);
 	cylinder->radius = cylinder->diameter / 2;
 	cylinder->radius2 = cylinder->radius * cylinder->radius;
