a4abb959213bc64ff96db4f8e1ce484fed07dfae
Jeonghwan Hwang <jhwang@c2r8s2.42seoul.kr>
2023-09-01 22:22:08 +0900

    add shadow

--- src/phong_lighting.c
+++ src/phong_lighting.c
@@ -1,5 +1,19 @@
-#include "datasheet.h"
-#include "utils_vec.h"
+#include "hit.h"
+
+int	is_shadow(t_rec *rec, t_data *data)
+{
+	t_rec	temp;
+	t_ray	light;
+	t_vec3	hitp;
+	
+	hitp = vadd_v(rec->intersect, vmul_s(rec->normal, EPSILON));
+	temp = *rec;
+	temp.tmax = vlen(vsub_v(data->light.point, temp.intersect));
+	light = ray_init(hitp, vsub_v(data->light.point, temp.intersect));
+	if (hit_object(data, light, &temp))
+		return (TRUE);
+	return (FALSE);
+}
 
 t_color	get_ambient(t_ambient *amb)
 {
@@ -17,7 +31,7 @@ t_color	get_specula(t_rec *rec, t_data *data)
 	from_light = vunit(vsub_v(rec->intersect, data->light.point));
 	to_view = vunit(vmul_s(data->camera.normal, -1));
 	reflect = vsub_v(from_light, vmul_s(rec->normal, vdot(from_light, rec->normal) * 2.0));
-	coef.x = 4;
+	coef.x = 2;
 	coef.y = 0.5;
 	coef.z = pow(fmax(vdot(to_view, reflect), 0.0), coef.x);
 	specula = vmul_s(vmul_s(data->light.color, coef.y), coef.z);
@@ -40,11 +54,16 @@ int	phong_lighting(t_rec *rec, t_data *data)
 {
 	t_color	light_color;
 	int		color;
+	double	shadow;
 
+	shadow = 1;
+	if (is_shadow(rec, data))
+		shadow = 0.6;
 	light_color = vset(0, 0, 0);
 	light_color = vadd_v(light_color, get_ambient(&data->ambient));
 	light_color = vadd_v(light_color, get_diffuse(rec, data));
 	light_color = vadd_v(light_color, get_specula(rec, data));
+	light_color = vmul_s(light_color, shadow);
 	light_color = vmin(vmul_v(light_color, rec->albedo), vset(1, 1, 1));
 	light_color = vmul_s(light_color, 255.999);
 	color = color_get_trgb(0x0, light_color.x, light_color.y, light_color.z);
